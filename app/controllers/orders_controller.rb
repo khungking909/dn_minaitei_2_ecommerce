46844965d34c7deab49e6702d50fbb72558c5f0f
Daiki <hothanhhung131@gmail.com>
2024-03-25 11:25:03 +0700

    UI order, add order detail page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@
 class OrdersController < ApplicationController
   before_action :parse_cart_data, only: :create
   before_action :find_products, only: :create
-  before_action :load_order, only: :cancel
+  before_action :load_order, only: %i(show cancel)
 
   def index
     @pagy, @orders = pagy(current_account.orders.order(created_at: :desc), items: Settings.PAGE_10)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,12 @@ class SessionsController < ApplicationController
     log_in(@account)
     remember(@account)
     flash[:success] = t("sessions.login_success")
-    redirect_to(login_path)
+    redirect_to(root_path)
+  end
+
+  def destroy
+    log_out
+    redirect_to(root_url)
   end
 
   private
