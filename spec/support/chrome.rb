18d0b3cf560b0df344fd7d0de5dd099d4a45cef5
Huynh Duc Truong <khungking12345@gmail.com>
2024-04-01 14:25:41 +0700

    init_rspec

---
+++ spec/support/chrome.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    if ENV["SHOW_BROWSER"] == "true"
+      driven_by :selenium_chrome
+    else
+      driven_by :selenium, using: :headless_chrome, screen_size: SIZE_1400x1400
+    end
+  end
+end
